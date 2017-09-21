cc442a9c3a891399cbebf3be3ddb5f70545d8435
adimango <alex.dimango@gmail.com>
2017-09-21 12:29:12 +0200

    Added media tests

--- insights-for-instagramTests/InstagramMediaTests.swift
+++ insights-for-instagramTests/InstagramMediaTests.swift
@@ -8,8 +8,31 @@
 
 import Quick
 import Nimble
+import Result
+import Alamofire
+@testable import insights_for_instagram
+import ObjectMapper
 
 class InstagramMediaTests: QuickSpec {
     override func spec() {
-    }
-}
+        
+        it("create from JSON") {
+            
+            let id = "id-xyz"
+            let code = "xyx"
+            let imageUrl = ""
+            let createdTime = Date()
+            let likesCount = ["count": 4]
+            let commentsCount = ["count": 3]
+            let type = "xy"
+            let json = ["id" : id, "code": code, "imageURL": imageUrl, "createdTime": createdTime, "likes": likesCount, "comments": commentsCount, "type": type] as [String : Any]
+            let media = InstagramMedia(JSON: json)
+            
+            expect(media?.id) == id
+            expect(media?.code) == code
+            expect(media?.likesCount) == 4
+            expect(media?.commentsCount) == 3
+            expect(media?.engagementCount) == 4 + 3
+        }
+        
+}}
